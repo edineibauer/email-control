222e6f78c262f8d9edc7bf5f3c89c4acdc0121d3
Edinei <galera.org@gmail.com>
2018-11-01 12:18:19 -0300

    notification fix

--- apiPublic/notification/sparkpost.php
+++ apiPublic/notification/sparkpost.php
@@ -1,18 +1,25 @@
 <?php
-$post = json_decode(file_get_contents('php://input'), true)[0]['msys']['message_event'];
-$type = $post['type'];
-$id = $post['transmission_id'];
+$content = file_get_contents('php://input');
+if(!empty($content)) {
+    $contentArray = json_decode($content, true);
+    if(!empty($contentArray[0]['msys']['message_event'])) {
+        $post = json_decode($content, true)[0]['msys']['message_event'];
+        $type = $post['type'];
+        $id = $post['transmission_id'];
 
-$up = new \ConnCrud\Update();
+        if($type === "delivery") {
+            $dados['email_recebido'] = 1;
+        } elseif($type === "open") {
+            $dados['email_aberto'] = 1;
+        } elseif($type === "click") {
+            $dados['email_clicado'] = 1;
+        } elseif($type === "spam_complaint") {
+            $dados['email_spam'] = 1;
+        }
 
-if($type === "delivery") {
-    $dados['email_entregue'] = 1;
-} elseif($type === "open") {
-    $dados['email_aberto'] = 1;
-} elseif($type === "click") {
-    $dados['email_clicado'] = 1;
-} elseif($type === "spam_complaint") {
-    $dados['email_spam'] = 1;
+        if(isset($dados)) {
+            $up = new \ConnCrud\Update();
+            $up->exeUpdate("email_envio", $dados, "WHERE transmission_id = :id", "id={$id}");
+        }
+    }
 }
-
-$up->exeUpdate("email_envio", $dados, "WHERE transmission_id = :id", "id={$id}");
